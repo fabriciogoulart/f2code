8f27b354a31573d0756df6eb0f8e6cab63802152
fabriciogoulart <fabricio.goulart.dev@gmail.com>
2017-03-26 11:17:54 -0300

    novas alteracoes.

--- js-default.php
+++
@@ -1,3 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/funcao.js"></script>
